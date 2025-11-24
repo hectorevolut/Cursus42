667b43c46e362884526086e049ced2c9d2a87701
Hector Calderon Hervas <hecalder@c3r5s3.42malaga.com>
2025-11-24 14:59:02 +0100

    24 11 2025

--- common-core/libft/libft.h
+++ common-core/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hecalder <hecalder@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 10:00:13 by hecalder          #+#    #+#             */
-/*   Updated: 2025/11/24 11:17:20 by hecalder         ###   ########.fr       */
+/*   Updated: 2025/11/24 14:50:08 by hecalder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,5 +73,8 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
